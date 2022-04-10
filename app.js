1887a18473354c4c9bec76a270de901d7cd291c3
seincode <pyleso58@gmail.com>
2022-04-11 01:01:07 +0900

    2. html 화면 띄워보기 [비효율적인 방법]

--- app.js
+++ app.js
@@ -2,15 +2,44 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) =>{
-    res.send("여기는 루트입니다.");
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        여기는 루트입니다.
+    </body>
+    </html>
+    `);
 })
 
 app.get("/login", (req, res) =>{
-    res.send("여기는 로그인 화면입니다.");
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <input type="text" placeholder="아이디"><br>
+            <input type="text" placeholder="비밀번호"><br>
+            <button>로그인</button>
+        </body>
+        </html>`
+    );
 })
 
 app.listen(3000, ()=> {
     console.log("서버 가동");
 });
 
-//npm install express -s 설치
+// 설명
+// 비효율 적으로 화면을 띄워 보았다.
