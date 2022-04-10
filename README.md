36695664a080c0ff667750f56ee94752e8c3f0c8
seincode <pyleso58@gmail.com>
2022-04-11 01:25:52 +0900

    6. 서버 모듈화 listen 작업

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# express-
+# express
+
+app.listen() 모듈화
+
+## 실행
+node .\bin\www.js 
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+"use strict"; 
+
 // 모듈
 const express = require("express");
 const app = express();
-
-const PORT = 3000; // 포트 설정
 
 // 라우팅
 const home = require("./routes/home");
@@ -13,7 +13,4 @@ app.set("view engine", "ejs") // 뷰 엔진
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
-app.listen(PORT, ()=> {
-    console.log("서버 가동");
-});
-
+module.exports = app;
