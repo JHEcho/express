de233addf51c0f6276afab00d01a7f6f7f58d76d
seincode <pyleso58@gmail.com>
2022-04-11 01:20:28 +0900

    5. 컨트롤러 분리 하기

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,16 @@
+"use strict"; 
+
+const home = (req, res) =>{
+    res.render("home/index");
+};
+
+const login = (req, res) =>{
+    res.render("home/login");
+};
+
+module.exports = {
+    home,
+    login,
+};
+
+// 컨트롤러 분리
--- routes/home/index.js
+++ routes/home/index.js
@@ -1,13 +1,12 @@
 "use strict"; 
+
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.render("home/index");
-})
+const ctrl = require("./home.ctrl");
 
-router.get("/login", (req, res) =>{
-    res.render("home/login");
-})
+router.get("/", ctrl.home);
+
+router.get("/login", ctrl.login);
 
 module.exports = router; // 외부 파일에서 사용하게 도와줌
