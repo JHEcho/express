94e3b610151c686ca3e89256dcd26eaa027c8be4
seincode <pyleso58@gmail.com>
2022-04-11 01:06:50 +0900

    3. ejs 모듈 사용해보기

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
 const express = require("express");
 const app = express();
 
+// 앱 세팅
+app.set("views", "./views"); //뷰 파일 경로
+app.set("view engine", "ejs") // 뷰 엔진
+
 app.get("/", (req, res) =>{
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        여기는 루트입니다.
-    </body>
-    </html>
-    `);
+    res.render("home/index");
 })
 
 app.get("/login", (req, res) =>{
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <input type="text" placeholder="아이디"><br>
-            <input type="text" placeholder="비밀번호"><br>
-            <button>로그인</button>
-        </body>
-        </html>`
-    );
+    res.render("home/login");
 })
 
 app.listen(3000, ()=> {
     console.log("서버 가동");
 });
-
-// 설명
-// 비효율 적으로 화면을 띄워 보았다.
