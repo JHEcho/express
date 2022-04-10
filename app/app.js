89a04ccfcf28b07e48ad1797ebae7386c82c1cbf
seincode <pyleso58@gmail.com>
2022-04-11 02:17:55 +0900

    9. 프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views"); //뷰 파일 경로
 app.set("view engine", "ejs") // 뷰 엔진
+app.use(express.static(`${__dirname}/src/public`)); // 디렉토리 위치 정적 경로 지정
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
