68b96801f6c109949379c321c27fdecebdcf4c9e
seincode <pyleso58@gmail.com>
2022-04-11 02:09:31 +0900

    8. 폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views"); //뷰 파일 경로
+app.set("views", "./src/views"); //뷰 파일 경로
 app.set("view engine", "ejs") // 뷰 엔진
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
